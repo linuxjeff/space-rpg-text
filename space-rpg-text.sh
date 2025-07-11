227b940cf7f3e6b90657792539887c71688bc273
Jefferson Vitor de Santana <computador.rp@gmail.com>
2025-07-10 22:00:36 -0300

    Tela inicial parcialmente pronta.

--- space-rpg-text.sh
+++ space-rpg-text.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 # v0.0.1
 # Este é um jogo de RPG em texto.
+
+function tela_inicial() {
+    OPCAO_TELA_INICIAL="" # Variável para tela inicial.
+
+    while [[ OPCAO_TELA_INICIAL -ne 2 ]]; do # While da tela inicial.
+      echo -e "SPACE RPG\n       TEXT"
+      echo -e "1 - Iniciar\n2 - Sair"
+      read -i "texto" OPCAO_TELA_INICIAL
+      if [ OPCAO_TELA_INICIAL != 1 -o OPCAO_TELA_INICIAL != 2 ]; then
+        echo "erro"
+        sleep 2
+      fi
+    done
+}
+
+tela_inicial
