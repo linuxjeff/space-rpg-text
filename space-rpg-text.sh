6aea047c4a31be22faf3958973d518bbc6c5b0ec
Jefferson Vitor de SAntana <computadores.rp@gmail.com>
2025-07-13 13:22:28 -0300

    Criado a tela de seleção de classe. Ela já esta funcional.

--- space-rpg-text.sh
+++ space-rpg-text.sh
@@ -2,6 +2,9 @@
 # v0.0.1
 # Este é um jogo de RPG em texto.
 
+NOME_DO_PERSONAGEM=""
+CLASSE_DO_PERSONAGEM=""
+
 function tela_inicial() {
   OPCAO_TELA_INICIAL="" # Variável para tela inicial.
 
@@ -27,11 +30,28 @@ function tela_inicial() {
 
 function tela_personagens() {
   PERSONAGEM_ESCOLHIDO=""
+  clear
   while true; do
-    echo -e "Classe: Engeiero Classe: Fuzileiro Classe: "
-    read PERSONAGEM_ESCOLHIDO
-    if [[ PERSONAGEM_ESCOLHIDO -eq 1 ]]; then
-      break
+    echo "| ID: 1            | ID: 2             | ID: 3             |"
+    echo "| Classe: Engeiero | Classe: Fuzileiro | Classe: Cientista |"
+    echo "| Ataque: 8        | Ataque: 10        | Ataque: 6         |"
+    echo "| Defesa: 10       | Defesa: 6         | Defesa: 8         |"
+    echo "| Velocidade: 6    | Velocidade: 8     | Velocidade: 10    |"
+    echo "Digite o ID do personagens ou zero para voltar a tela inicail."
+    echo -n ">>> " ; read -t 60 PERSONAGEM_ESCOLHIDO
+    if [[ PERSONAGEM_ESCOLHIDO -eq "" ]]; then
+      PERSONAGEM_ESCOLHIDO="0"
+    fi
+    if [[ $PERSONAGEM_ESCOLHIDO -eq 1 || $PERSONAGEM_ESCOLHIDO -eq 2 ]]; then
+      echo "Tela nome do personagem."
+      exit 0
+    elif [[ PERSONAGEM_ESCOLHIDO -eq "0" ]]; then
+      tela_inicial
+    else
+      clear
+      echo "Não a este ID de personagens."
+      sleep 3
+      clear
     fi
   done
 }
