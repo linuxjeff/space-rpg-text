e9405a8894066469297a69c07c5262bd9a687931
Jefferson Vitor de SAntana <computadores.rp@gmail.com>
2025-07-29 16:19:22 -0300

    Guarda o total de experiencia do personagem.

--- space-rpg-text.sh
+++ space-rpg-text.sh
@@ -4,7 +4,7 @@
 
 NOME_DO_PERSONAGEM="" # Guarda o nome do personagem no nivel Global
 CLASSE_DO_PERSONAGEM=""   # Guarda a classe do personagem em nivel global.
-NUMERO_TOTAL_DE_XP=""
+NUMERO_TOTAL_DE_XP=""     # Guarda o total de experiencia do personagem.
 SOMA_DE_QUILOMETROS=""
 NUMERO_DO_EVENTO=""
 
