7be89ac30df56124ae5695c01f4c817b1822815e
Jefferson Vitor de Santana <computador.rp@gmail.com>
2025-07-28 22:17:22 -0300

    Mudanças na tela_inicial e tela_personagem

--- space-rpg-text.sh
+++ space-rpg-text.sh
@@ -9,51 +9,60 @@ CLASSE_DO_PERSONAGEM=""
 function tela_inicial() {
   OPCAO_TELA_INICIAL="" # Variável para tela inicial.
 
-  while true; do # While da tela inicial.
+  clear
+
+  echo -e "SPACE RPG\n       TEXT"
+  echo -e "1 - Iniciar\n2 - Sair"
+  echo -n ">>> " ; read OPCAO_TELA_INICIAL
+
+  OPCAO_TELA_INICIAL="${OPCAO_TELA_INICIAL^^}"
+
+  case $OPCAO_TELA_INICIAL in
+    1 | "I" | "INICIAR" )
+    tela_personagens
+      ;;
+    2 | "S" | "SAIR" )
+    exit 0
+      ;;
+    * )
     clear
-    echo -e "SPACE RPG\n       TEXT"
-    echo -e "1 - Iniciar\n2 - Sair"
-    echo -n ">>> " ; read OPCAO_TELA_INICIAL
-    # If para verifiicar conteudo da variável OPCAO_TELA_INICIAL.
-    if [[ OPCAO_TELA_INICIAL -eq 2 ]]; then
-      break
-    elif [[ OPCAO_TELA_INICIAL -eq 1 ]]; then
-      tela_personagens
-    else
-      clear
-      echo "Opção invalida!"
-      sleep 2
-    fi
-  done
+    echo "Opção invalida!"
+    sleep 2
+    tela_inicial
+      ;;
+  esac
 }
 
 
 function tela_personagens() {
-  PERSONAGEM_ESCOLHIDO=""
   clear
-  while true; do
-    echo "| ID: 1            | ID: 2             | ID: 3             |"
-    echo "| Classe: Engeiero | Classe: Fuzileiro | Classe: Cientista |"
-    echo "| Ataque: 8        | Ataque: 10        | Ataque: 6         |"
-    echo "| Defesa: 10       | Defesa: 6         | Defesa: 8         |"
-    echo "| Velocidade: 6    | Velocidade: 8     | Velocidade: 10    |"
-    echo "Digite o ID do personagens ou zero para voltar a tela inicail."
-    echo -n ">>> " ; read -t 60 PERSONAGEM_ESCOLHIDO
-    if [[ PERSONAGEM_ESCOLHIDO -eq "" ]]; then
-      PERSONAGEM_ESCOLHIDO="0"
-    fi
-    if [[ $PERSONAGEM_ESCOLHIDO -eq 1 || $PERSONAGEM_ESCOLHIDO -eq 2 || $PERSONAGEM_ESCOLHIDO -eq 3 ]]; then
-      tela_nome
-      CLASSE_DO_PERSONAGEM="$PERSONAGEM_ESCOLHIDO"
-    elif [[ PERSONAGEM_ESCOLHIDO -eq "0" ]]; then
-      tela_inicial
-    else
-      clear
-      echo "Não a este ID de personagens."
-      sleep 3
-      clear
-    fi
-  done
+
+  echo "| ID: 1            | ID: 2             | ID: 3             |"
+  echo "| Classe: Engeiero | Classe: Fuzileiro | Classe: Cientista |"
+  echo "| Ataque: 8        | Ataque: 10        | Ataque: 6         |"
+  echo "| Defesa: 10       | Defesa: 6         | Defesa: 8         |"
+  echo "| Velocidade: 6    | Velocidade: 8     | Velocidade: 10    |"
+  echo "Digite o ID do personagens ou (S)air para voltar a tela inicail."
+  echo -n ">>> " ; read PERSONAGEM_ESCOLHIDO
+
+  PERSONAGEM_ESCOLHIDO="${PERSONAGEM_ESCOLHIDO^^}"
+
+  case $PERSONAGEM_ESCOLHIDO in
+    1 | 2 | 3)
+    CLASSE_DO_PERSONAGEM=$PERSONAGEM_ESCOLHIDO
+    tela_nome
+      ;;
+  "S" )
+    tela_inicial
+      ;;
+    * )
+    clear
+    echo "Não a este ID de personagens."
+    sleep 3
+    clear
+    tela_personagens
+esac
+
 }
 
 
@@ -64,18 +73,56 @@ function tela_nome() {
   echo -n "Digite o nome do seu personagem: " ; read NOME_ESCOLHIDO
   clear
   echo "O nome escolhido foi: $NOME_ESCOLHIDO"
-  #echo -e "Manter - 1\nTrocar - 2"
-  #echo -n ">>> " ; read ESCOLHA
-  #if [[ $ESCOLHA -eq 1 ]]; then
-  #  NOME_DO_PERSONAGEM=$NOME_ESCOLHIDO
-  #elif [[ $ESCOLHA -eq 2 ]]; then
-  #  tela_nome
-  #else
-  #  clear
-  #  echo "Opção invalida!"
-  #  sleep 2
-  #  tela_nome
-  #fi
+
+  tela_da_sinopse
+}
+
+
+function tela_da_sinopse() {
+
+  clear
+
+  echo -e "
+   Você entrou na atmosfera de Aurora no sistema estelar de Vanini. Em alguns
+   segundos a sua nave começa a dar vários alarmes de panes em alguns sistemas
+   críticos, o  pouso controlado da nave se torna impossível, mas você consegue
+   pousar com apenas um ferimento. Você checa no mapa e vê que a cidade está a
+   200 quilômetros. Sem outro meio para chegar até lá você terá que caminhar.
+   Você pega seus kits médicos e sua arma e começa a caminhar.
+
+\tSistema Estelar: Vanini
+\t\tMundo: Aurora
+\t\t\tAtmosfera: Respirável
+\t\t\tTipos de Biomas: Aridos
+"
+
+read -p "Pressione enter para continuar..."
+
+tela_da_mecanica
+}
+
+function tela_da_mecanica() {
+
+  clear
+
+  echo "
+    Como você está a 200 quilômetros de distância do seu objetivo a cada
+    quilômetro um dado será jogado trazendo para ti um evento aleatório.
+
+    Eventos:
+      - Nada: você teve sorte e só seguirá para o próximo quilômetro.
+      - Batalha: você vai enfrentar um alien.
+      - Locais: você encontra um local que pode ser explorado.
+  "
+
+  read -p "Pressione enter para continuar..."
+
+  tela_do_jogador
+
+}
+
+function tela_do_jogador () {
+  echo
 }
 
 tela_inicial
